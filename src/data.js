86f9c07acfc4a708704dfa579e85ce65ab29c0b4
tyagi-krishna <kt159875321@gmail.com>
2023-12-30 15:35:26 +0530

    First Commit

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const projectData = [
+	{
+		projectName: "Blockchain based Certificate Generation and Verification",
+		projectDate: "November 2022",
+		projectDescription:
+			"Revolutionize certificate handling with our project! Using Ethereum blockchain, we secure certificates with Hardhat, ReactJS for a user-friendly interface, and IPFS servers for decentralized storage. This ensures tamper-proof certificates, instant verification and transparent traceable transactions",
+		tags: ["Hardhat", "ReactJS", "NextJS", "Solidity", "IPFS"],
+		link: "",
+		repo: "https://github.com/tyagi-krishna/Blockchain-Certificate-App",
+	},
+	{
+		projectName: "Stock Market Domain with Price Predictions",
+		projectDate: "November 2022",
+		projectDescription:
+			" I’ve created a Stock Market Price Prediction website using Django, python and Nsetools API. With real-time market data, it employs a Linear Regression model from Scikit-learn for accurate predictions. The website offers a user-friendly interface with interactive visualizations using Matplotlib. Beyond predictions, it also showcase’s comprehensive features of Indian stock market companies, providing insights for strategic investment decisions.",
+		tags: [
+			"Django",
+			"Scikit Learn",
+			"Matplotlib",
+			"NseTools",
+			"Pandas",
+			"Numpy",
+		],
+		link: "",
+		repo: "https://github.com/tyagi-krishna/Stock-Market-Prices-Prediction",
+	},
+	{
+		projectName: "Blockchain based Funding System",
+		projectDate: "November 2022",
+		projectDescription:
+			"I've pioneered a revolutionary Blockchain-Based Funding System, utilizing Solidity, Hardhat, and Next.js. Smart contracts in Solidity ensure secure and transparent transactions, while Hardhat streamlines development. The Next.js frontend offers a user-friendly experience. This decentralized system eliminates intermediaries, supporting asset tokenization and providing an immutable transaction history. With integrated wallets and real-time analytics, my project transforms fundraising, fostering trust and efficiency in the process",
+		tags: ["ReactJS", "NextJS", "Hardhat", "Solidity"],
+		link: "",
+		repo: "https://github.com/tyagi-krishna/Charity-Funding-System",
+	},
+	{
+		projectName: "Blog website with REST API",
+		projectDate: "November 2022",
+		projectDescription:
+			"I've crafted a dynamic Blog Website with a robust REST API using Django and Django REST Framework. This platform offers seamless content creation and consumption. The combination of Django's simplicity and REST Framework's flexibility ensures a user-friendly experience. With features like authentication, data serialization, and endpoints, my project empowers users to effortlessly manage, create, and share content,elevating the blogging experience.",
+		tags: ["Django", "REST FrameWork", "SQLite"],
+		link: "",
+		repo: "https://github.com/tyagi-krishna/Blog-with-API",
+	},
+];
